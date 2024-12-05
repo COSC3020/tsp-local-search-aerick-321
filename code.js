5c3805ad8df888a9ed6dd17df8eab67a6506e567
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-12-05 14:48:17 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,15 +1,14 @@
 function tsp_ls(distance_matrix) {
 //Initialize random route
-   let route1 = []; 
+   let route1; 
 //Set incumbent route as the random route
 //Repeat until stopping condition met:
-    for(let noimprove =0; noimprove < 100) {
-    //Select random i and k
-        function randomik(n){
+   function randomik(n){
             let i = Math.floor(Math.random() * (n-1));
             let k = Math.floor(Math.random() *(n-i-1) + i+1);
             return [i, k]
         }
+    for(let noimprove =0; noimprove < 100) {
     //Perform 2-opt swap
        function optSwap(route, i, k) {
             let start = route.slice(0, i);
@@ -24,14 +23,12 @@ function tsp_ls(distance_matrix) {
      /*If new route is shorter:
         Update incumbent route
         Return length of shortest route*/
-        if(newroute < route1){
+        if(newroute <= route1){
             newroute = route1;
             route= nowroute;
-            return route.length;
             noimprove = 0;
         }
         else{
-             return route.length
             noimprove++;
         }
     }
